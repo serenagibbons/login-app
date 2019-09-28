5a7bf72a072197eb2c147020b511afccc62b4e06
Serena Gibbons <serenalgibbons@gmail.com>
2019-09-28 11:35:29 -0400

    Basic registration page design

--- app/src/main/java/com/example/loginapp/RegistrationActivity.java
+++ app/src/main/java/com/example/loginapp/RegistrationActivity.java
@@ -3,6 +3,7 @@ package com.example.loginapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+    }
+
+    public void registerUser(View view) {
+
     }
 }
