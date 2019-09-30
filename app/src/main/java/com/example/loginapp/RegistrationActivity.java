50cbce01319673843f71236c6a2bd4c2c7a8f473
Serena Gibbons <serenalgibbons@gmail.com>
2019-09-30 15:58:53 -0400

    Test for valid DOB

--- app/src/main/java/com/example/loginapp/RegistrationActivity.java
+++ app/src/main/java/com/example/loginapp/RegistrationActivity.java
@@ -14,11 +14,14 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.zip.DataFormatException;
 
 public class RegistrationActivity extends AppCompatActivity {
 
     EditText e1, e2, e3, e4, e5;
-    boolean valid = true;
+    boolean valid;
     String fName, lName, dob, email, pass;
 
 
@@ -34,6 +37,9 @@ public class RegistrationActivity extends AppCompatActivity {
     }
 
     public void registerUser(View view) {
+        // reset valid boolean
+        valid = true;
+
         if (e1.getText().toString().isEmpty() || e2.getText().toString().isEmpty()
                 || e3.getText().toString().isEmpty() || e4.getText().toString().isEmpty()
                 || e5.getText().toString().isEmpty()) {
@@ -42,6 +48,7 @@ public class RegistrationActivity extends AppCompatActivity {
             return;
         }
 
+        // set variables using user's input
         fName = e1.getText().toString();
         lName = e2.getText().toString();
         dob = e3.getText().toString();
@@ -57,6 +64,7 @@ public class RegistrationActivity extends AppCompatActivity {
             return;
         }
 
+        // create new intent and send data to the LoginActivity
         Intent intent = new Intent(this, LoginActivity.class);
         intent.putExtra("First Name", fName);
         intent.putExtra("Last Name", lName);
@@ -68,10 +76,24 @@ public class RegistrationActivity extends AppCompatActivity {
     }
 
     public boolean validateInput() {
+        // check first name must be between 3 and 30 characters
         if (fName.length() < 3 || fName.length() > 30) {
             Toast.makeText(this, getResources().getString(R.string.toast_name), Toast.LENGTH_LONG).show();
             valid = false;
         }
+
+        // try to parse dob to check for valid date of birth
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy", Locale.US);
+            sdf.setLenient(false);
+            sdf.parse(dob);
+        }
+        catch (Exception e) {
+            Toast.makeText(this, getResources().getString(R.string.toast_date), Toast.LENGTH_LONG).show();
+            valid = false;
+        }
+
+        // check for valid email address
         if (!(email.contains("@") || email.endsWith(".edu")
                 || email.endsWith(".com") || email.endsWith(".org") || email.endsWith(".net"))) {
             Toast.makeText(this, getResources().getString(R.string.toast_email), Toast.LENGTH_LONG).show();
